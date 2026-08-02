7c5e7b38e15846507af47bed65e7f23bc82ba18e
repunit11 <kenken22352235@gmail.com>
2026-08-02 07:04:20 +0000

    feat: 正常なバックエンドのみ転送先へ反映

--- l4lb/control/controller.go
+++ l4lb/control/controller.go
@@ -1,21 +1,16 @@
 package control
 
 import (
-	"net"
+	"fmt"
+	"net/http"
 	"net/netip"
+	"time"
 
 	"github.com/yzp0n/ncdn/l4lb/l4lbdrv"
 )
 
 type DataPlane interface {
 	Apply(state l4lbdrv.ForwardingState) error
-}
-
-type DestinationEntries []DestinationEntry
-
-type DestinationEntry struct {
-	IPAddr       netip.Addr
-	HardwareAddr net.HardwareAddr
 }
 
 type Controller struct {
@@ -28,5 +23,35 @@ func New(dp DataPlane, desiredState l4lbdrv.ForwardingState) *Controller {
 }
 
 func (c *Controller) Reconcile() error {
-	return c.dp.Apply(c.desiredState)
+	state := c.desiredState
+	state.Dests = c.healthyDests()
+	return c.dp.Apply(state)
+}
+
+func (c *Controller) healthyDests() l4lbdrv.DestinationEntries {
+	if len(c.desiredState.Dests) == 0 {
+		return nil
+	}
+	healthy := make(l4lbdrv.DestinationEntries, 0, len(c.desiredState.Dests))
+	healthy = append(healthy, c.desiredState.Dests[0])
+	for _, backend := range c.desiredState.Dests[1:] {
+		if isHealthy(backend.IPAddr) {
+			healthy = append(healthy, backend)
+		}
+	}
+	return healthy
+}
+
+func isHealthy(ip netip.Addr) bool {
+	client := &http.Client{
+		Timeout: time.Second,
+	}
+	url := fmt.Sprintf("http://%s:8889/statusz", ip)
+	resp, err := client.Get(url)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK
 }
